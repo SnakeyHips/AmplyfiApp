062c7788293ab1e649ba731be6b39eff9a899511
jgough <jacob.gough@web.com>
2019-05-20 13:34:36 +0100

    removed bootstrap and jquery

--- AmplyfiApp/ClientApp/src/components/Progress.js
+++ AmplyfiApp/ClientApp/src/components/Progress.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+});
+
 class Progress extends Component {
   render () {
+    const { classes } = this.props;
+
     return (
       <Grid container justify="center" alignContent="center">
-        <CircularProgress color="secondary" />
+        <CircularProgress className={classes.progress} color="secondary" />
       </Grid>
     );
   }
 }
-export default Progress;
+
+Progress.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Progress);
